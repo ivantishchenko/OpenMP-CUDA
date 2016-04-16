7211f536e203f783fa69b6ac92596eec3c4da3f5
Tishchenko Ivan <tishchi1@spinelli.org.aalto.fi>
2016-04-16 14:00:04 +0300

    MF1 runs 3.936

--- mf1/mf.cc
+++ mf1/mf.cc
@@ -6,10 +6,12 @@ void mf(int ny, int nx, int hy, int hx, const float* in, float* out) {
     int limit_y, limit_x, beg_x, beg_y;
     std::vector<float>::iterator max_half;
    
+    int row_window, row_matr, win_size_half;
+   
     for ( int i = 0; i < ny; i++ ) {
         limit_y = std::min(ny, i + hy + 1);
         beg_y = std::max(0, i - hy);
-        
+        row_matr = i * nx;
         for ( int j = 0; j < nx; j++ ) {
             
             window_elements.clear();
@@ -19,19 +21,22 @@ void mf(int ny, int nx, int hy, int hx, const float* in, float* out) {
             
 
             for ( int k = beg_y; k < limit_y; k++ ) {
+                row_window = k * nx;
                 for ( int l = beg_x; l < limit_x; l++ ) {
-                    window_elements.push_back(in[l + k * nx]);
+                    window_elements.push_back(in[l + row_window]);
                 }
             }
 
-            std::nth_element(window_elements.begin(), window_elements.begin() + window_elements.size()/2, window_elements.end());
+            win_size_half = window_elements.size()/2;
+
+            std::nth_element(window_elements.begin(), window_elements.begin() + win_size_half, window_elements.end());
             
             if ( window_elements.size() % 2 == 1 ) {
-                 out[j + i * nx] = window_elements[window_elements.size()/2];
+                 out[j + row_matr] = window_elements[win_size_half];
             }
             else {
-                 max_half = std::max_element(window_elements.begin(), window_elements.begin() + window_elements.size()/2);
-                 out[j + i * nx] = (window_elements[window_elements.size()/2] + *max_half) / 2 ;
+                 max_half = std::max_element(window_elements.begin(), window_elements.begin() + win_size_half);
+                 out[j + row_matr] = (window_elements[win_size_half] + *max_half) / 2 ;
             }
         }
     }   
