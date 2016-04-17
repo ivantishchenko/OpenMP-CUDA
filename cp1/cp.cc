59b192f61444e9accebc691f2b7cdd6c97d17d52
Tishchenko Ivan <tishchi1@tavi.org.aalto.fi>
2016-04-17 13:06:31 +0300

    CP1 scored 7.145

--- cp1/cp.cc
+++ cp1/cp.cc
@@ -2,46 +2,38 @@
 #include <cmath> 
 
 void correlate(int ny, int nx, const float* data, float* result) {
-    double mean;
-    double** matrix = new double*[ny];
+    //double mean;
+    double sum;
+    double ** matrix = new double*[ny];
         for (int i = 0; i < ny; ++i)
     matrix[i] = new double[nx];
     
     for ( int i = 0; i < ny; i++ ) {
-        mean = 0;
+        sum = 0;
         for ( int j = 0; j < nx; j++ ) {
-            mean += data[j + i * nx];
+            sum += data[j + i * nx];
         }
-        mean /= nx;
+        sum = sum / nx;
         for ( int j = 0; j < nx; j++ ) {
-            matrix[i][j]= data[j + i * nx] - mean;
+            matrix[i][j] = data[j + i * nx] - sum;
         }
     }
     
-    //for ( int i = 0; i < ny; i++ ) {
-    //    for ( int j = 0; j < nx; j++ ) {
-    //        matrix[i][j] = matrix[i][j] / sum;
-    //    }
-    //}
-    
     for ( int j = 0; j < ny; j++ ) {
         for ( int i = j; i < ny; i++ ) {
-            double sa = 0.0;
-            double sb = 0.0;
             double sab = 0.0;
-            double saa = 0.0;
-            double sbb = 0.0;
+            double saa = 0;
+            double sbb = 0;
             for (int x = 0; x < nx; x++) {
                 double a = matrix[i][x];
                 double b = matrix[j][x];
-
                 sab += a * b;
                 saa += a * a;
                 sbb += b * b;
             }
-            double r = nx * sab - sa * sb;
-            r /= std::sqrt(nx * saa - sa * sa);
-            r /= std::sqrt(nx * sbb - sb * sb);
+            double r = nx * sab ;
+            r /= std::sqrt(nx * saa);
+            r /= std::sqrt(nx * sbb);
             result[i + j * ny] = r;
         }
     }
