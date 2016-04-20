72ed965abe17b0b970f22bedc9b5c56460d3ab46
Tishchenko Ivan <tishchi1@pulu.org.aalto.fi>
2016-04-20 21:11:58 +0300

    CP2 achieved 5.745

--- cp2/cp.cc
+++ cp2/cp.cc
@@ -1,8 +1,44 @@
 #include "cp.h"
+#include <cmath> 
+#include <vector>
 
 void correlate(int ny, int nx, const float* data, float* result) {
-    // FIXME
-    for (int i = 0; i < ny * ny; ++i) {
-        result[i] = 0.0f;
+    //double mean;
+    //double sum, sum_sqr;
+    
+    std::vector<std::vector<double>> matrix(ny, std::vector<double>(nx));
+   
+    // first optimization
+    for ( int i = 0; i < ny; i++ ) {
+        double sum = 0;
+        for ( int j = 0; j < nx; j++ ) {
+            sum += data[j + i * nx];
+        }
+        sum = sum / nx;
+        double sum_sqr = 0;
+        for ( int j = 0; j < nx; j++ ) {
+            matrix[i][j] = data[j + i * nx] - sum;
+            sum_sqr += matrix[i][j] * matrix[i][j];
+        }
+        sum_sqr = std::sqrt(sum_sqr);
+        for ( int j = 0; j < nx; j++ ) {
+            matrix[i][j] /= sum_sqr;            
+        }
     }
+  
+    #pragma omp parallel for schedule(static,1)
+    for ( int j = 0; j < ny; j++ ) {
+        for ( int i = j; i < ny; i++ ) {
+            double sab = 0.0;
+            for (int x = 0; x < nx; x++) {
+                double a = matrix[i][x];
+                double b = matrix[j][x];
+                sab += a * b;
+            }
+            double r = nx * sab;
+            r /= nx;
+            result[i + j * ny] = r;
+        }
+    }
+
 }
