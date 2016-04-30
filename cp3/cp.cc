b47e0f6a8acb0344762798713cac154c89e529a3
Tishchenko Ivan <tishchi1@flamingo.org.aalto.fi>
2016-04-30 19:36:34 +0300

    CP3 3.632

--- cp3/cp.cc
+++ cp3/cp.cc
@@ -21,7 +21,7 @@ void correlate(int ny, int nx, const float* data, float* result) {
     int nx_full = nx / 4;
     int _nx = std::ceil(nx / 4.0);
     double4_t * x = double4_alloc(_nx * ny); 
-     //std::cout << " NX " << _nx << std::endl;     
+     //std::cout << " NX " << _nx << std::endl;    
     for (int i = 0; i < ny; i++) { 
         for (int j = 0, k = 0; j < nx_full ; j++, k += 4) { 
             double4_t tmp = { data[k + 0 + i * nx], data[k + 1 + i * nx], data[k + 2 + i * nx], data[k + 3 + i * nx] };
@@ -60,7 +60,14 @@ void correlate(int ny, int nx, const float* data, float* result) {
         }
         double4_t sum_sqr = double4_0;
         double sum_one = (sum[0] + sum[1] + sum[2] + sum[3]) / nx;
-        for ( int j = 0; j < _nx; j++ ) {
+        //CHANGES
+        int j = 0;
+        for ( j = 0; j < nx_full; j++ ) {
+            
+            x[i * _nx  + j] = x[i * _nx  + j] - sum_one;
+            sum_sqr += x[i * _nx  + j] * x[i * _nx  + j];
+            
+            /*
             if ( j == _nx - 1 ) {
                 for ( int p = 0; p < 4; p++) {
                     if ( x[i * _nx  + j][p] != 0 ) x[i * _nx  + j][p] = x[i * _nx  + j][p] - sum_one;
@@ -68,10 +75,20 @@ void correlate(int ny, int nx, const float* data, float* result) {
             } 
             else {
                 x[i * _nx  + j] = x[i * _nx  + j] - sum_one;
-            }
+            }*/
               
-            sum_sqr += x[i * _nx  + j] * x[i * _nx  + j];
+           
         }
+        
+        //if ( j == _nx - 1 ) {
+            for ( int a = 0; a < nx % 4; a++ ) {
+               // std::cout << " T = " << nx - nx % 4 + a << std::endl; 
+                x[i * _nx  + j][a] = x[i * _nx  + j][a] - sum_one;
+                sum_sqr[a] += x[i * _nx  + j][a] * x[i * _nx  + j][a];
+            }
+       // }
+        //std::cout << " T = "<< std::endl; 
+            
         
         double sum_sqr_one = std::sqrt(sum_sqr[0] + sum_sqr[1] + sum_sqr[2] + sum_sqr[3]);
         for ( int j = 0; j < _nx; j++ ) {
