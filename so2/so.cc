f343faeee8167d60ce025517152f70d119948d2a
Tishchenko Ivan <tishchi1@hypersteeni.org.aalto.fi>
2016-05-13 16:05:13 +0300

    SO2 almost working

--- so2/so.cc
+++ so2/so.cc
@@ -1,7 +1,94 @@
 #include "so.h"
 #include <algorithm>
+#include <omp.h>
+#include <iostream>
+ void quickSort ( data_t * arr, int left, int right, int nthrds) {
+     
+     
+        if (nthrds < 2) {
+            std::sort(arr + left, arr + right); 
+            return;
+        }
+        
+      int i = left, j = right;
 
-void psort(int n, data_t* data) {
-    // FIXME: make this more efficient with parallelism
-    std::sort(data, data + n);
-}
+      data_t tmp;
+
+      data_t pivot = arr[(left + right) / 2];
+
+    
+
+      /* partition */
+
+      while (i <= j) {
+
+            while (arr[i] < pivot)
+                  i++;
+
+            while (arr[j] > pivot)
+                  j--;
+
+            if (i <= j) {
+
+                  tmp = arr[i];
+
+                  arr[i] = arr[j];
+
+                  arr[j] = tmp;
+
+                  i++;
+
+                  j--;
+
+            }
+
+      }
+
+ 
+
+      /* recursion */
+      
+      if (left < j) {
+          #pragma omp task
+            quickSort(arr, left, j, nthrds / 2);
+      }
+            
+
+      if (i < right) {
+          #pragma omp task
+          quickSort(arr, i, right, nthrds / 2);
+      }
+
+            
+ }
+ void psort(int n, data_t* data) {
+     int nthrds = omp_get_max_threads();
+#pragma omp parallel 
+#pragma omp single 
+   {
+       quickSort(data, 0, n, nthrds);
+   }
+   /*
+    int nthrds = omp_get_max_threads();
+    int parts = n / nthrds; 
+    #pragma omp parallel for schedule(dynamic) 
+    for ( int i = 0; i < nthrds; i++) {
+        int beg = parts * i;
+        int end = beg + parts;
+        std::sort(data + beg, data + end);  
+    }
+    
+    while ( nthrds > 1 ) {
+        #pragma omp parallel num_threads(nthrds) 
+        {
+            int parts = n / nthrds; 
+            int id = omp_get_thread_num();
+            int beg = parts * id;
+            int end = beg + parts;
+            int mid = beg + parts / 2;
+            std::inplace_merge(data + beg, data + mid, data + end);
+        }
+        nthrds = nthrds / 2;
+    }
+    std::inplace_merge(data, data + n / 2, data + n);*/
+ }
